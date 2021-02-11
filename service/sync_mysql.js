301cbd93eae12a2ed6abb8ff9114fbdd5f0da408
argon1025 <argon1025@gmail.com>
2021-02-11 19:48:35 +0900

    Modify Mysql 모듈을 리팩토링 했습니다

--- service/sync_mysql.js
+++ service/sync_mysql.js
@@ -12,37 +12,34 @@
 const mysql = require('sync-mysql');
 const settings = require('./settings');
 
-class database {
-    constructor(){
-        this.connection = new mysql(settings.db_config);
+class Database {
+    constructor() {
+        this.CONNECTION = new mysql(settings.db_config);
     }
-    // 사용자 정의 쿼리 실행
-    // 특정 데이터를 가져올때
-    async get_query(sql){
-        const result = this.connection.query(sql);
-        console.log(result);
-        console.log(`쿼리 질의 결과 ${result['affectedRows']}`);
-        if(!result[0]){
+
+    async get_query(sql) {
+        const result = await this.CONNECTION.query(sql);
+
+        if (!result[0]) {
             //질의 결과가 없을경우
             throw 'DB No results';
-        }else{
+        } else {
             //질의 결과가 있을경우
             return result;
         }
     }
-    // 특정 데이터의 존재유무, 또는 삽입
-    async insert_query(sql){
-        const result = this.connection.query(sql);
-        console.log(`쿼리 질의 결과 ${result['affectedRows']}`);
-        if(!result['affectedRows']){
+
+    async insert_query(sql) {
+        const result = await this.CONNECTION.query(sql);
+
+        if (!result['affectedRows']) {
             //질의 결과가 없을경우
             throw result;
-        }else{
+        } else {
             //질의 결과가 있을경우
             return result;
         }
-        
     }
 }
 
-module.exports = new database();
+module.exports = new Database();
