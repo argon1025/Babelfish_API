97bd01cdaef6f7e17b5471d7ae6fd63ef562eb96
argon1025 <argon1025@gmail.com>
2021-02-22 13:58:35 +0900

    Modify 에러 핸들러 모듈의 에러코드가 잘못된 부분을 수정했습니다

--- service/ErrorHandler.js
+++ service/ErrorHandler.js
@@ -28,7 +28,7 @@ class ErrorHandler {
                     'Token authentication failed': { 'msgCode': '4', 'msg': 'Token invalid or expired', 'code': '401' },
                     'no permission': { 'msgCode': 'n2-2', 'msg': 'Unable to modify other user information', 'code': '401' },
                     'no affectedRows': { 'msgCode': 'n2-3', 'msg': 'DB No results', 'code': '400' },
-                    'Over Max Notes': { 'msgCode': 'n2-4', 'msg': 'Over Max Notes', 'code': '401' }
+                    'Over Max Notes': { 'msgCode': 'n2-5', 'msg': 'Over Max Notes', 'code': '401' }
                 }
             },
             '/:userid/notes/:noteid': {
